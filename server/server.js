1b84d28f286ab3db226c1e1ab25d5097c3c62766
ramsankar <ramsankar.aloor@gmail.com>
2023-05-17 10:35:42 +0530

    health api added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 import publicRouter from "./publicRouter.js";
 import privateRouter from "./privateRouter.js"
+import cors from 'cors'
 
 dotenv.config()
 
@@ -12,13 +13,14 @@ const app = express();
 
 app.listen(4000);
 
+app.use(cors())
 app.use(express.json());
 app.use('/public', publicRouter)
 app.use('/private', privateRouter)
 
-// app.get('/health', function(req, res){
-//     res.status(200).send('OK');
-// }
+app.get('/health', function(req, res){
+    res.status(200).send('OK');
+})
 
 
 
